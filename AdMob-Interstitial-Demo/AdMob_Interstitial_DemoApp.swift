b0af333915a2e227ae950fd4c503a811b703dbfc
SNQ-2001 <taishin05109@gmail.com>
2022-05-10 15:07:23 +0900

    Initial Commit

---
+++ AdMob-Interstitial-Demo/AdMob_Interstitial_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AdMob_Interstitial_DemoApp.swift
+//  AdMob-Interstitial-Demo
+//
+//  Created by 宮本大新 on 2022/05/10.
+//
+
+import SwiftUI
+
+@main
+struct AdMob_Interstitial_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
